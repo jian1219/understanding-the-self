e0799e32e50b51935c861fb5e856ab61905fd4ba
jian1219 <jian70227@gmail.com>
2024-11-28 11:27:41 +0800

    new add content home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,8 @@ import "../css/Home.css"
 import logo_brain from "../images/logo brain.jpg"
 import right_arrow from "../icons/right-arrow.png"
 
+import utspic1 from "../images/uts 1.jpg"
+
 function Home() {
 
   const [menuOpen, setMenuOpen] = useState(false);
@@ -43,6 +45,36 @@ function Home() {
               <li><a href="#option3">Spots</a></li>
 
             </ul>
+          </div>
+
+
+            {/* Content */}
+          <div className='parent-content'>
+            <div>
+              <img className='w-[100%]' src={utspic1} alt="" />
+            </div>
+            <div className='child-content'>
+              <p>Understanding the self is the ability to gain insight into one's own behaviors, attitudes, 
+                strengths, and weaknesses. It's a dynamic process that's shaped by personal experiences, 
+                social interactions, cultural influences, and individual reflection.</p>
+              <h4 className='mt-2'>Some aspects of understanding the self include:</h4>
+              <ul>
+                <li>Self-concept: The overall idea of who we think we are. It's developed through interactions with 
+                  others and social comparison. </li>
+                <li>Self-esteem: Based on the evaluations and judgments we make about our self-concept. </li>
+                <li>Self-awareness: Having self-awareness can help us become better decision-makers, 
+                  communicate more clearly, and understand things from multiple perspectives. </li>
+              </ul>
+              <h4 className='mt-2'>Some benefits of understanding the self include:</h4>
+              <ul>
+                <li>Being able to influence outcomes</li>
+                <li>Having more self-confidence</li>
+                <li>Being able to communicate with clarity and intention</li>
+                <li>Being able to understand things from multiple perspectives</li>
+                <li>Being able to free ourselves from assumptions and biases </li>
+              </ul>
+            </div>
+
           </div>
           
       </div>
